c99c30b13ba4633032b7139caa0bdbb689f00867
Martinskatvedt <martinskatvedt@gmail.com>
2021-10-28 11:11:01 +0200

    inital port from techtalks2

---
+++ client/src/components/home/Overview.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  flex: 1;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  flex-flow: row wrap;
+`;
+
+
+const Overview: FC = () => {
+  return (
+    <Wrapper>
+      Loading...
+    </Wrapper>
+  );
+};
+
+export default Overview;
---
+++ client/src/components/home/SamarbeidsPartnere.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Samarbeidspartnere: FC = () => {
+  return (
+    <Wrapper>
+      Loading...
+    </Wrapper>
+  );
+};
+
+export default Samarbeidspartnere;
