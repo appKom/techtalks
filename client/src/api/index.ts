566befa83202ca0ab800891ecd97627f15ecf2a8
Jørgen Reimers <jorgenreim@gmail.com>
2022-01-19 10:54:35 +0100

    Fix BASE_URL

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.BASE_URL || 'localhost:8080/api/v1';
+const BASE_URL = process.env.BASE_URL || 'http://localhost:8080/api/v1';
 export const COMPANIES_URL = BASE_URL + '/companies';
 export const EVENTS_URL = BASE_URL + '/event';
 
