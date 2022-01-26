1386dca303eec8360043878df7570fb532d3d71e
Fredrik Skaug <fredskau@stud.ntnu.no>
2022-01-26 11:31:27 +0100

    Link to program page

---
+++ client/src/pages/Program.tsx
@@ -0,0 +1,17 @@
+import React, {FC} from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div``;
+
+const Program: FC = () => {
+  return (
+    <Wrapper>
+      <h2 id="program">Program</h2>
+      <p>
+        <h3>Kommer snart...</h3>
+      </p>
+    </Wrapper>
+  );
+};
+
+export default Program;
