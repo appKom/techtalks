9452968ab635f389726318c540cd88749db2f46c
Fredrik Skaug <fredskau@stud.ntnu.no>
2022-01-30 16:14:35 +0100

    api call from OW

--- client/src/components/home/SamarbeidsPartnere.tsx
+++ client/src/components/home/SamarbeidsPartnere.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -7,9 +7,45 @@ const Wrapper = styled.div`
 `;
 
 const Samarbeidspartnere: FC = () => {
+  interface Company {
+    id: number,
+    name: string,
+}
+
+  const [companies, setCompanies] = useState<any | null>([]);
+
+  function handleNewCompany(company:Company | any) {
+    setCompanies((prevArray: any) => [
+      ...prevArray,
+      {
+        id: company.id,
+        name: company.name
+      }]);
+  }
+
+  const OwCompaniesUrl = 'https://old.online.ntnu.no/api/v1/companies/'
+  const AttendingIDs = [
+    1, 2, 3, 4, 5
+  ]
+  useEffect(() => {
+    async function fetchCompaniesAPI(id: number) {let response = await fetch(OwCompaniesUrl + id + '/')
+      response = await response.json()
+      handleNewCompany(response)
+    }
+    AttendingIDs.map((id) => (
+      fetchCompaniesAPI(id)
+    ))
+  }, [])
+
+  console.log(companies)
   return (
     <Wrapper>
-      Loading...
+      {companies.map((data:Company) => (
+        <>
+          <p>{data.id}</p>
+          <p>{data.name}</p>
+        </>
+      ))}
     </Wrapper>
   );
 };
